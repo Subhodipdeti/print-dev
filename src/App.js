728489eebda4204ccf5de4c954211b9d46cf5577
Subhodip Deti <v-subhodip.deti@rebelfoods.com>
2021-08-18 17:55:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,221 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import JSPM from "jsprintmanager";
+// import qz from "qz-tray";
+import PrintJobForRawCommands from "./PrintJobForRawCommands";
+// import Printers from "./Printers";
+// import Scanning from './Scanning';
+import PrinterInfoSample from './PrinterInfoSample';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class PrintingRawCommandsSample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      job: null,
+      clientPrinter: null,
+      printerCommands: "",
+      printerCommandsCodePage: -1,
+    };
+  }
+
+  componentDidMount() {
+    // qz.websocket
+    //   .connect()
+    //   .then(() => {
+    //     return qz.printers.find();
+    //   })
+    //   .then((printers) => {
+    //     console.log(printers);
+    //     let config = qz.configs.create("Thermal Printer H58 Printer USB");
+    //     return qz.print(config, [
+    //       {
+    //         type: "pixel",
+    //         format: "html",
+    //         flavor: "plain",
+    //         data: "<h1>Hello JavaScript!</h1>",
+    //       },
+    //     ]);
+    //   })
+
+    //   .then(() => {
+    //     return qz.websocket.disconnect();
+    //   })
+    //   .then(() => {
+    //     // process.exit(0);
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //     // process.exit(1);
+    //   });
+
+    JSPM.JSPrintManager.auto_reconnect = true;
+    JSPM.JSPrintManager.start();
+
+    JSPM.JSPrintManager.MainApp = this;
+
+    JSPM.JSPrintManager.getPrinters().then(function (myPrinters) {
+      console.log("=====>>> myPrinters", myPrinters);
+    }).catch(err => console.log('====>>>', err))
+
+    JSPM.JSPrintManager.WS.onOpen = function () {
+      JSPM.JSPrintManager.MainApp.jspmWsStatusChanged(
+        JSPM.WSStatus[JSPM.JSPrintManager.WS.status]
+      );
+
+      JSPM.JSPrintManager.MainApp.printersInfoChanged(null);
+      //get client installed printers with detailed info
+      JSPM.JSPrintManager.getPrintersInfo().then(function (printersList) {
+        JSPM.JSPrintManager.MainApp.printersInfoChanged(printersList);
+      });
+    };
+
+    JSPM.JSPrintManager.WS.onStatusChanged = function () {
+      JSPM.JSPrintManager.MainApp.jspmWsStatusChanged(
+        JSPM.WSStatus[JSPM.JSPrintManager.WS.status]
+      );
+    };
+
+    JSPM.JSPrintManager.WS.onClose = function () {
+      JSPM.JSPrintManager.MainApp.jspmWsStatusChanged(
+        JSPM.WSStatus[JSPM.JSPrintManager.WS.status]
+      );
+    };
+
+    let os = (function () {
+      var ua = navigator.userAgent.toLowerCase();
+      return {
+        isWindows: /(windows|win32|win64)/.test(ua),
+        isLinux: /(linux)/.test(ua),
+        isIntelMac: /(intel mac)/.test(ua),
+        isRPi: /(Linux arm)/.test(ua),
+      };
+    })();
+
+    if (os.isWindows) {
+      this.OS = "win";
+    } else if (os.isLinux) {
+      this.OS = "linux";
+    } else if (os.isIntelMac) {
+      this.OS = "mac";
+    } else if (os.isRPi) {
+      this.OS = "rpi";
+    } else {
+      this.OS = "other";
+    }
+  }
+
+  getPrinters() {
+    // JSPM.JSPrintManager.auto_reconnect = true;
+    // JSPM.JSPrintManager.start();
+
+    return new Promise((ok, err) => {
+      let printers = [];
+      if (JSPM.JSPrintManager.websocket_status == JSPM.WSStatus.Open) {
+        console.log("=OK");
+        JSPM.JSPrintManager.getPrinters()
+          .then(function (myPrinters) {
+            console.log("===>>>>", myPrinters);
+            printers = myPrinters;
+            console.log(printers);
+            ok(printers);
+          })
+          .catch((e) => err(e));
+      } else {
+        console.warn("JSPM WS not open");
+        ok(printers);
+      }
+    });
+  }
+
+  doPrinting() {
+    if (this.state.job) {
+      let cpj = this.state.job;
+      cpj.sendToClient();
+    }
+  }
+
+  updateJob() {
+    var cpj = new JSPM.ClientPrintJob();
+    cpj.clientPrinter = this.state.clientPrinter;
+    cpj.printerCommands = this.state.printerCommands;
+    cpj.printerCommandsCodePage = this.state.printerCommandsCodePage;
+    //no need to re-render
+    this.state.job = cpj;
+  }
+
+  onPrinterChange(newPrinter) {
+    //No need to re-render!
+    this.state.clientPrinter = newPrinter;
+    this.updateJob();
+  }
+
+  onPrinterCommandsChange(newPrinterCommands) {
+    //No need to re-render!
+    this.state.printerCommands = newPrinterCommands;
+    this.updateJob();
+  }
+
+  onPrinterCommandsCodePageChange(newCodepage) {
+    //No need to re-render!
+    this.state.printerCommandsCodePage = newCodepage;
+    this.updateJob();
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <Scanning /> */}
+        <PrinterInfoSample />
+        <h1 onClick={this.getPrinters}>Hmmm</h1>
+        <div className="row">
+          <div className="col-md-1">
+            <button
+              className="btn btn-dark btn-lg"
+              onClick={() => this.props.setSample(0)}
+            >
+              <i className="fa fa-arrow-left" />
+            </button>
+          </div>
+          <div className="col-md-11">
+            <h2 className="text-center">
+              <i className="fa fa-barcode" />
+              &nbsp;Raw Data Printing
+            </h2>
+            <hr />
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="row">
+            {/* <Printers
+              JobContentType={0}
+              onPrinterChange={this.onPrinterChange.bind(this)}
+            /> */}
+          </div>
+          <div className="row">
+            <PrintJobForRawCommands
+              jobIndex={0}
+              onPrinterCommandsChange={this.onPrinterCommandsChange.bind(this)}
+              onPrinterCommandsCodePageChange={this.onPrinterCommandsCodePageChange.bind(
+                this
+              )}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <br />
+            <div className="text-center">
+              <button
+                className="btn btn-success btn-lg"
+                onClick={this.doPrinting.bind(this)}
+              >
+                <i className="fa fa-print" /> Print Now...
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default PrintingRawCommandsSample;
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,44 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+// qz.websocket
+//     .connect()
+//     .then(() => {
+//       return qz.printers.find();
+//     })
+//     .then((printers) => {
+//       console.log(printers);
+//       let config = qz.configs.create("192.168.0.101");
+//       return qz.print(config, [
+//         {
+//           type: "pixel",
+//           format: "html",
+//           flavor: "plain",
+//           data: "<h1>Hello JavaScript!</h1>",
+//         },
+//       ]);
+//     })
+//     .then(() => {
+//       return qz.websocket.disconnect();
+//     })
+//     .then(() => {
+//       // process.exit(0);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       // process.exit(1);
+//     });
+
+
+// function onInit() {
+//     // var _this = this;
+//     JSPM.JSPrintManager.auto_reconnect = true;
+//     JSPM.JSPrintManager.start();
+//     JSPM.JSPrintManager.WS.onStatusChanged = function () {
+//       // _this.getPrinters().then((p)=>{
+//       //   _this.printers = p;
+//       // });
+//     };
+//   }
